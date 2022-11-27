b4b2ddf612936cd9c88b901562f0ea33dd98b8f9
MetalAI <tevesb50golden@yahoo.com>
2022-11-27 16:34:25 -0600

    Updated Front Page and About Page
    - Included links to subpages and official Harbor Freight website on Home Page
    
    - About Page now contains a disclaimer

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,12 +3,12 @@
 export default function AboutPage(){
     return (
         <div className="About">
-            <h1><center>About</center></h1>
+            <center><h1>About</h1>
             <p>We are a small group of six people who created this non-profit project. It was made for COSC 4336 (Software Development)
                 at the University of Texas at Tyler. The website was made in Next.js using JavaScript, and the API is hosted with Spring 
                 using Java.
             </p><br></br>
-            <center><h2>Members</h2>
+            <h2>Members</h2>
             <h3>Back End Team</h3>
             <p>Justin Blalock<br></br>
             Nathan Farrar</p>
@@ -17,9 +17,11 @@ export default function AboutPage(){
             <p>Benjamin Teves Jr.<br></br>
             Vincent Hilario</p>
 
-            <h3>Quality Assurance</h3>
+            <h3>Quality Assurance Team</h3>
             <p>Madison Davis<br></br>
-            Max Cabell</p></center>
+            Max Cabell</p>
+            
+            <small>Disclaimer: The Harbor Freight Database Team is not affiliated with Harbor Freight in any shape or form.</small></center>
         </div>
     )
 }
