38ac1187085d2b4a2aa6786f12c30fa316031bc2
MetalAI <tevesb50golden@yahoo.com>
2022-11-27 14:07:43 -0600

    Updated About Page
    Added member names to credit the people who worked on this project

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,17 +3,23 @@
 export default function AboutPage(){
     return (
         <div className="About">
-            <h1>About us:</h1>
+            <h1><center>About</center></h1>
             <p>We are a small group of six people who created this non-profit project. It was made for COSC 4336 (Software Development)
                 at the University of Texas at Tyler. The website was made in Next.js using JavaScript, and the API is hosted with Spring 
                 using Java.
-            </p>
-            <h1>Members</h1>
-            <h2>Back End Team</h2>
+            </p><br></br>
+            <center><h2>Members</h2>
+            <h3>Back End Team</h3>
+            <p>Justin Blalock<br></br>
+            Nathan Farrar</p>
 
-            <h2>Front End Team</h2>
-            
-            <h2>Quality Assurance</h2>
+            <h3>Front End Team</h3>
+            <p>Benjamin Teves Jr.<br></br>
+            Vincent Hilario</p>
+
+            <h3>Quality Assurance</h3>
+            <p>Madison Davis<br></br>
+            Max Cabell</p></center>
         </div>
     )
 }
