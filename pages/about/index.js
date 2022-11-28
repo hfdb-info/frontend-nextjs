8fa3c46bb50e3c843d006f3a1073e09adbf30572
MetalAI <tevesb50golden@yahoo.com>
2022-11-27 19:13:26 -0600

    updated About Page

--- pages/about/index.js
+++ pages/about/index.js
@@ -4,9 +4,8 @@ export default function AboutPage(){
     return (
         <div className="About">
             <center><h1>About</h1>
-            <p>We are a small group of six people who created this non-profit project. It was made for COSC 4336 (Software Development)
-                at the University of Texas at Tyler. The website was made in Next.js using JavaScript, and the API is hosted with Spring 
-                using Java.
+            <p>We are a small group of six people who created this project for our COSC 4336 (Software Development) class
+                at the University of Texas at Tyler. The website was made using Postgres, Spring for API hosted using Java, and Next.js with JavaScript.
             </p><br></br>
             <h2>Members</h2>
             <h3>Back End Team</h3>
