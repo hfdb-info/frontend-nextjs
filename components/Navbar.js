2e330d0b44a5f79665f074b2c927f04fd1b8fb33
MetalAI <tevesb50golden@yahoo.com>
2022-11-27 18:54:16 -0600

    Updated Home Page
    - Rearranged/reworded some text
    - Text colors for logo and welcome sentence

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,8 +8,8 @@ const Navbar = () => {
         <h1>HFDB</h1>
       </div>
       <Link href="/">Home </Link>
-      <Link href="/about"> About </Link>
       <Link href="/search/"> Search</Link>
+      <Link href="/about"> About </Link>
     </nav>
 );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <div className="homePage">
     <center><h2>Welcome to the Harbor Freight Database!</h2>
-    <p>This database gives an insight on what products from Harbor Freight are currently on discount. <br></br>
+    <p>This database details the price changes on Harbor Freight's products. <br></br>
     <br></br>
-      <big>For more information regarding the latter, visit our <a href="http://localhost:3000/about">About Page</a></big> <br></br><br></br>
-      <big>Ready to start looking for the hottest deals from Harbor Freight? <a href="http://localhost:3000/search">Search now!</a></big><br></br>
+      <big>Ready to start looking for the hottest deals from Harbor Freight? <a href="http://localhost:3000/search">Search now</a></big><br></br>
       <br></br>
       <br></br>
-      Looking for the original Harbor Freight website? <a href="https://www.harborfreight.com/">Click here</a>.</p>
+      For more information about this website, visit our <a href="http://localhost:3000/about">About Page</a>. <br></br>
+      Looking for the original Harbor Freight website? <a href="https://www.harborfreight.com/">Click here</a></p>
       
     </center></div>
   )
